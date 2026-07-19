25a45c42c3162e819a34bd143a510ee5bfc9cdb7
Thomas Muenz <temuenz@t-online.de>
2026-07-19 11:28:05 +0200

    using explain analyze, verbose

--- test/sql/tc016.sql
+++ test/sql/tc016.sql
@@ -5,7 +5,7 @@
 explain (analyze,verbose) select * from sample.employee where lastname between 'LUTZ' and 'LEE' order by lastname;
 select * from sample.employee where lastname between 'LEE' and 'LUTZ' order by lastname;
 --
-explain select * from sample.employee where salary between 10000.00 and 60000.00 order by salary;
+explain (analyze,verbose) select * from sample.employee where salary between 10000.00 and 60000.00 order by salary;
 select * from sample.employee where salary between 10000.00 and 60000.00 order by salary;
 --
 -- END of TC016
