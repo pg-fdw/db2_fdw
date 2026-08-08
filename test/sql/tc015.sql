ff7e13e3dd793f06bb9802495a83980a234ed2a8
Thomas Muenz <temuenz@t-online.de>
2026-08-08 15:09:10 +0200

    fix issue #122

--- test/sql/tc015.sql
+++ test/sql/tc015.sql
@@ -19,6 +19,59 @@ SELECT PERCENTILE_DISC(0.5)
        WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Disc
        FROM sample.tbcfileentry;
 
+EXPLAIN (verbose)
+SELECT PERCENTILE_CONT(0.11) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_1
+     , PERCENTILE_CONT(0.12) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_2
+     , PERCENTILE_CONT(0.13) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_3
+     , PERCENTILE_CONT(0.14) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_4
+     , PERCENTILE_CONT(0.15) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_5
+     , PERCENTILE_CONT(0.16) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_6
+     , PERCENTILE_CONT(0.17) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_7
+     , PERCENTILE_CONT(0.18) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_8
+     , PERCENTILE_CONT(0.19) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_9
+     , PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_0
+     , PERCENTILE_CONT(0.21) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_1
+       FROM sample.tbcfileentry;
+
+SELECT PERCENTILE_CONT(0.11) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_1
+     , PERCENTILE_CONT(0.12) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_2
+     , PERCENTILE_CONT(0.13) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_3
+     , PERCENTILE_CONT(0.14) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_4
+     , PERCENTILE_CONT(0.15) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_5
+     , PERCENTILE_CONT(0.16) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_6
+     , PERCENTILE_CONT(0.17) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_7
+     , PERCENTILE_CONT(0.18) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_8
+     , PERCENTILE_CONT(0.19) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_9
+     , PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_0
+     , PERCENTILE_CONT(0.21) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_1
+       FROM sample.tbcfileentry;
+
+EXPLAIN (verbose)
+SELECT PERCENTILE_DISC(0.11) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_1
+     , PERCENTILE_DISC(0.12) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_2
+     , PERCENTILE_DISC(0.13) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_3
+     , PERCENTILE_DISC(0.14) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_4
+     , PERCENTILE_DISC(0.15) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_5
+     , PERCENTILE_DISC(0.16) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_6
+     , PERCENTILE_DISC(0.17) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_7
+     , PERCENTILE_DISC(0.18) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_8
+     , PERCENTILE_DISC(0.19) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_9
+     , PERCENTILE_DISC(0.20) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_0
+     , PERCENTILE_DISC(0.21) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_1
+       FROM sample.tbcfileentry;
+
+SELECT PERCENTILE_DISC(0.11) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_1
+     , PERCENTILE_DISC(0.12) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_2
+     , PERCENTILE_DISC(0.13) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_3
+     , PERCENTILE_DISC(0.14) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_4
+     , PERCENTILE_DISC(0.15) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_5
+     , PERCENTILE_DISC(0.16) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_6
+     , PERCENTILE_DISC(0.17) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_7
+     , PERCENTILE_DISC(0.18) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_8
+     , PERCENTILE_DISC(0.19) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_0_9
+     , PERCENTILE_DISC(0.20) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_0
+     , PERCENTILE_DISC(0.21) WITHIN GROUP (ORDER BY tbcfientid) AS Perc_Cont_1_1
+       FROM sample.tbcfileentry;
 --
 -- END of TC015
 --
--- test/sql/tc019.sql
+++ test/sql/tc019.sql
@@ -1,6 +1,7 @@
 --
 -- TC019: test using a query as table for a fdw table
 --
+DROP FOREIGN TABLE IF EXISTS sample.percent;
 CREATE FOREIGN TABLE sample.percent (
     percent NUMERIC
 )
@@ -11,6 +12,26 @@ OPTIONS ( table '(SELECT PERCENTILE_CONT(0.9) WITHIN GROUP (ORDER BY TBCFIENTID)
 --
 explain (analyze,verbose) select * from sample.percent;
 select * from sample.percent;
+
+DROP FOREIGN TABLE IF EXISTS sample.percent3;
+CREATE FOREIGN TABLE sample.percent3 (
+    p10 NUMERIC
+   ,p11 NUMERIC
+   ,p12 NUMERIC
+   ,p13 NUMERIC
+)
+SERVER sample
+OPTIONS ( table '(SELECT PERCENTILE_CONT(0.10) WITHIN GROUP (ORDER BY TBCFIENTID) AS p10'\
+                ',PERCENTILE_CONT(0.11) WITHIN GROUP (ORDER BY TBCFIENTID) AS p11'\
+                ',PERCENTILE_CONT(0.12) WITHIN GROUP (ORDER BY TBCFIENTID) AS p12'\
+                ',PERCENTILE_CONT(0.13) WITHIN GROUP (ORDER BY TBCFIENTID) AS p13'\
+                ' FROM DB2INST1.TBCFILEENTRY)');
+--
+\d+ sample.percent3;
+--
+explain (analyze,verbose) select * from sample.percent3;
+select * from sample.percent3;
+
 --
 -- END of TC019
 --
