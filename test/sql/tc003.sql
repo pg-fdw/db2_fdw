8bf962c8a745160e653d6581dab02061848dd2d0
Thomas Muenz <temuenz@t-online.de>
2026-08-23 13:12:00 +0200

    enrich tc003

--- test/sql/tc003.sql
+++ test/sql/tc003.sql
@@ -2,10 +2,16 @@
 -- TC003: Run a simple join
 --
 \d+ sample.employee;
+select * from sample.employee;
+--
 \d+ sample.sales;
+select * from sample.sales;
 -- test a simple join
 explain (analyze,verbose) select * from sample.employee a, sample.sales b where a.lastname = b.sales_person;
 select * from sample.employee a, sample.sales b where a.lastname = b.sales_person;
+--
+explain (analyze,verbose) select * from sample.employee a join sample.sales b on a.lastname = b.sales_person;
+select * from sample.employee a join sample.sales b on a.lastname = b.sales_person;
 --
 -- End of TC003
 --
